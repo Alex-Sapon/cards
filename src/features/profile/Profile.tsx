f574342e4db50720091110aa158ef2d9beca2e0b
Alex <aliakseishulha2021@gmail.com>
2022-06-27 16:05:53 +0300

    profile inc

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,7 +1,84 @@
+import {ChangeEvent, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {ThunkDispatch} from "redux-thunk";
+
+import {Avatar} from "@mui/material";
+import TextField from "@mui/material/TextField";
+import LinearProgress from "@mui/material/LinearProgress";
+
+import Button from "../../common/button/Button";
+import userPhoto from "../../assets/images/avatar.jpg"
+import {AppStateType} from "../../components/app/store";
+import {ActionsType, logoutTC, ProfileStateType, updateProfileTC} from "./profileReducer";
+import {useStyles} from "./styles";
+
+export const useAppDispatch = () => useDispatch<ThunkDispatch<AppStateType, unknown, ActionsType>>();
+
 export const Profile = () => {
-    return (
-        <div>
-            <h2>Profile</h2>
-        </div>
-    )
-}
+	const styles = useStyles()
+	const dispatch = useAppDispatch()
+
+	let [isEditMode, setEditMode] = useState(false)
+	let [title, setTitle] = useState('');
+
+	const {name, status, email, publicCardPacksCount} = useSelector<AppStateType, ProfileStateType>(state => state.profile)
+
+	const activateEditMode = () => {
+		setEditMode(true);
+		setTitle(name)
+	}
+
+	const activateViewMode = () => {
+		setEditMode(false);
+	}
+
+	const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+		setTitle(e.currentTarget.value)
+	}
+
+	const updateName = () => {
+		dispatch(updateProfileTC(name))
+		setEditMode(false)
+	}
+
+	const logoutHandler = () => {
+		dispatch(logoutTC())
+	};
+
+	return (
+		<div>
+			{status === 'loading' && <LinearProgress/>}
+			<div className={styles.profileContainer}>
+				<div className={styles.profileWrapper}>
+					<div className={styles.profileItems}>
+						<Avatar
+							alt="Remy Sharp"
+							src={userPhoto}
+							sx={{width: 96, height: 96}}
+						/>
+						<Button onClick={logoutHandler}>Log out</Button>
+					</div>
+					<div className={styles.name}>
+						{isEditMode
+							?
+							<TextField
+								onChange={changeTitle}
+								value={title}
+								label="Nickname"
+								variant="standard"
+								autoFocus
+								onBlur={activateViewMode}/>
+							: <span onDoubleClick={activateEditMode}>{name}</span>}
+					</div>
+					<div>
+						<div className={styles.profileTitle}>Имя: {email}</div>
+						<div className={styles.profileTitle}>Количество создоваемых колод: {publicCardPacksCount}</div>
+
+					</div>
+					<Button onClick={updateName} className={styles.button}>Save</Button>
+				</div>
+			</div>
+		</div>
+	)
+};
+
