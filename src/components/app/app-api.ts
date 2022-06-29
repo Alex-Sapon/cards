3946193e56efe8b3837f5e599c09a2005bdacd23
Alex <aliakseishulha2021@gmail.com>
2022-06-29 20:07:30 +0300

    test instance v4

--- src/components/app/app-api.ts
+++ src/components/app/app-api.ts
@@ -1,13 +1,9 @@
 import axios, {AxiosResponse} from 'axios';
-
-export const instanceLogin = axios.create({
-    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
-    withCredentials: true,
-});
+import {instance} from "../../assets/settings/instance-api";
 
 export const appAPI = {
     me() {
-        return instanceLogin.post<any, AxiosResponse<IMeResponse>, {}>(`auth/me`);
+        return instance.post<any, AxiosResponse<IMeResponse>, {}>(`auth/me`);
     }
 };
 
