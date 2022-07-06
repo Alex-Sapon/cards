e693b4757b11e1caeeddc45054793bdb8f435f44
SHDGMSQ <sh.dg.msq@mail.ru>
2022-07-06 21:58:19 +0300

    commit v1.0

--- src/features/packsList/showPacks/ShowPacks.tsx
+++ src/features/packsList/showPacks/ShowPacks.tsx
@@ -4,7 +4,7 @@ import styles from './ShowPacks.module.css';
 import Button from '../../../common/button/Button';
 import {useAppDispatch, useAppSelector} from '../../../app/store';
 import {fetchCardPacksDima} from '../Dima/reducer/packsListDimaReducer';
-import {useEffect, useState } from 'react';
+import {MouseEvent, useEffect, useState} from 'react';
 import { useDebounce } from '../Dima/debounce/debounce';
 
 export const ShowPacks = () => {
@@ -14,40 +14,48 @@ export const ShowPacks = () => {
     const user_id = useAppSelector(state => state.login._id)
 
     const [value, setValue] = React.useState<number[]>([0, 110]);
-
-    const [searchTerm, setSearchTerm] = useState('');
-    const [results, setResults] = useState([]);
+    const [doRequest, setDoRequest] = useState(false)
+    const [switchOn, setSwitchOn] = useState(false)
 
     const debouncedSearchTerm = useDebounce(value, 500);
     useEffect(
         () => {
-            if (debouncedSearchTerm) {
+            if (debouncedSearchTerm && doRequest) {
                 dispatch(fetchCardPacksDima({min: value[0], max: value[1]}))
+                setDoRequest(false)
             }
         },
-        [debouncedSearchTerm]
+        [debouncedSearchTerm, doRequest]
     );
 
-
-
-    const handleChange = (event: Event, newValue: number | number[]) => {
-        setValue(newValue as number[]);
-        console.log(value[0]);
-    };
-
+    const handleChange = (event: Event, newValue: number | number[])  => {
+        if (Array.isArray(newValue))
+            setValue(newValue as number[])
+    }
     const myPacksClickHandler = () => {
-       dispatch(fetchCardPacksDima({user_id}))
+        dispatch(fetchCardPacksDima({user_id}))
     }
     const allPacksClickHandler = () => {
-       dispatch(fetchCardPacksDima({}))
+        dispatch(fetchCardPacksDima({}))
     }
 
+    const handleClick = (e: MouseEvent<HTMLSpanElement>) => {
+        if (e.type === 'mouseup') {
+            setDoRequest(true)
+        }
+    }
     return (
         <div className={styles.left_bar}>
             <h3 className={styles.left_bar_title}>Show packs cards</h3>
             <div className={styles.button_group}>
-                <Button onClick={myPacksClickHandler} disabled={status === 'loading'} >My</Button>
-                <Button onClick={allPacksClickHandler} disabled={status === 'loading'}>All</Button>
+                <Button
+                    onClick={myPacksClickHandler}
+                    disabled={status === 'loading'}
+                    >My</Button>
+                <Button
+                    onClick={allPacksClickHandler}
+                    disabled={status === 'loading'}
+                >All</Button>
             </div>
             <h3 className={styles.left_bar_subtitle}>Number of cards</h3>
             <div className={styles.slider}>
@@ -57,6 +65,8 @@ export const ShowPacks = () => {
                     onChange={handleChange}
                     valueLabelDisplay="on"
                     max={110}
+                    onMouseUp={handleClick}
+                    disabled={status === 'loading'}
                 />
             </div>
         </div>
