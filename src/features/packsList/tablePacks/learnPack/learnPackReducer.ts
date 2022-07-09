f8d0afb558d6dd66dad33e06337c4983d1842be1
Aleksandr_Saponchik <alexsapon@gmail.com>
2022-07-09 23:30:59 +0300

    feat: add learnPackReducer

---
+++ src/features/packsList/tablePacks/learnPack/learnPackReducer.ts
@@ -0,0 +1,19 @@
+import {CardType} from '../../../packName/apiCardName/apiPackName';
+
+const initState: LearnPackStateType = {
+    cards: [] as CardType[],
+}
+
+export const learnPackReducer = (state: LearnPackStateType = initState, action: LearnPackActions): LearnPackStateType => {
+  switch(action.type) {
+      default:
+          return state;
+  }
+}
+
+// types
+export type LearnPackActions = any
+
+type LearnPackStateType = {
+
+}
