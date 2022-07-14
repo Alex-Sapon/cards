2ded936cd6e6ac50674252a6bda62c74e5200c13
SHDGMSQ <sh.dg.msq@mail.ru>
2022-07-14 20:55:29 +0300

    commit working version v_1.1

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -13,113 +13,104 @@ import userPhoto from '../../assets/images/avatar.jpg';
 import {useAppDispatch, useAppSelector} from '../../app/store';
 import {SmallAvatar, useStyles} from './styles';
 import {PATH} from '../../enums/path';
-import Badge from "@mui/material/Badge";
+import Badge from '@mui/material/Badge';
 import {logoutTC, updateUserDataTC} from '../login/reducer/loginReducer';
 
 
 export const Profile = () => {
-	const styles = useStyles()
-	const dispatch = useAppDispatch()
+    const styles = useStyles();
+    const dispatch = useAppDispatch();
 
-	const status = useAppSelector(state => state.app.status)
-	const avatar = useAppSelector(state => state.login.avatar)
-	const name = useAppSelector(state => state.login.name)
-	const email = useAppSelector(state => state.login.email)
-	const publicCardPacksCount = useAppSelector(state => state.login.publicCardPacksCount)
-	const isLoggedIn = useAppSelector(state => state.login.isLoggedIn)
+    const avatar = useAppSelector(state => state.login.avatar);
+    const name = useAppSelector(state => state.login.name);
+    const email = useAppSelector(state => state.login.email);
+    const publicCardPacksCount = useAppSelector(state => state.login.publicCardPacksCount);
+    const isLoggedIn = useAppSelector(state => state.login.isLoggedIn);
 
-	let [isEditMode, setEditMode] = useState<boolean>(false)
-	let [title, setTitle] = useState<string>(name);
+    let [isEditMode, setEditMode] = useState<boolean>(false);
+    let [title, setTitle] = useState<string>(name);
 
-	const activateEditMode = () => {
-		setEditMode(true);
-		setTitle(title)
-	}
+    const activateEditMode = () => {
+        setEditMode(true);
+        setTitle(title);
+    };
 
-	useEffect(() => {
-		dispatch(updateUserDataTC(title, avatar || userPhoto))
-	}, [])
+    useEffect(() => {
+        dispatch(updateUserDataTC(title, avatar || userPhoto));
+    }, []);
 
-	const activateViewMode = () => {
-		setEditMode(false);
-	}
+    const activateViewMode = () => {
+        setEditMode(false);
+    };
 
-	const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
-		if (e.currentTarget.value.length > 20) return
-		setTitle(e.currentTarget.value)
-	}
+    const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.currentTarget.value.length > 20) return;
+        setTitle(e.currentTarget.value);
+    };
 
-	const onKeyPressHandler = (e: React.KeyboardEvent<HTMLDivElement>) => {
-		if (e.key === 'Enter') {
-			activateViewMode()
-		}
-	}
+    const onKeyPressHandler = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'Enter') {
+            activateViewMode();
+        }
+    };
 
-	const updateName = () => {
-		dispatch(updateUserDataTC(title, avatar || userPhoto))
-		setEditMode(false)
-	}
+    const updateName = () => {
+        dispatch(updateUserDataTC(title, avatar || userPhoto));
+        setEditMode(false);
+    };
 
-	const logoutHandler = () => {
-		dispatch(logoutTC())
-	};
+    const logoutHandler = () => {
+        dispatch(logoutTC());
+    };
 
-	if (!isLoggedIn) {
-		return <Navigate to={PATH.LOGIN}/>
-	}
+    if (!isLoggedIn) {
+        return <Navigate to={PATH.LOGIN}/>;
+    }
 
-	return (
-		<div>
-			{status === 'loading'
-				? (<div><Stack spacing={2} direction="row">
-					<CircularProgress color="secondary"/>
-				</Stack></div>)
-				: (<div className={styles.profileContainer}>
-						<div className={styles.profileLogOutButton}>
-							<Button onClick={logoutHandler}>Log out</Button>
-						</div>
-						<div className={styles.profileWrapper}>
-							<div className={styles.title}>Personal Information</div>
-							<Stack direction="row" spacing={2}>
-								<Badge
-									overlap="circular"
-									anchorOrigin={{vertical: 'bottom', horizontal: 'right'}}
-									badgeContent={
-										<SmallAvatar alt="Remy Sharp" src={iconPhoto}/>}>
-									<Avatar alt="Travis Howard" src={avatar || userPhoto} sx={{width: 96, height: 96}}/>
-								</Badge>
-							</Stack>
-							<div>
-								{isEditMode
-									? <TextField
-										onKeyPress={onKeyPressHandler}
-										onChange={changeTitle}
-										value={title}
-										label="Nickname"
-										variant="standard"
-										autoFocus
-										onBlur={activateViewMode}
-										InputProps={{
-											startAdornment: (<AccountCircle/>),
-											endAdornment: (<ClearIcon/>)
-										}}/>
-									: <div className={styles.nickName}>Nickname:
-										<div className={styles.name}>{title}</div>
-										{!!activateViewMode && <ModeEditIcon onClick={activateEditMode} fontSize={"small"}/>}
-									</div>}
-							</div>
-							<div className={styles.information}>
-								<div className={styles.description}>Contact email: {email}</div>
-								<div>Number of decks created: {publicCardPacksCount}</div>
-							</div>
-							<div>
-								<Button className={styles.button} onClick={updateName}>Save</Button>
-							</div>
-						</div>
+    return (
+        <div className={styles.profileContainer}>
+            <div className={styles.profileLogOutButton}>
+                <Button onClick={logoutHandler}>Log out</Button>
+            </div>
+            <div className={styles.profileWrapper}>
+                <div className={styles.title}>Personal Information</div>
+                <Stack direction="row" spacing={2}>
+                    <Badge
+                        overlap="circular"
+                        anchorOrigin={{vertical: 'bottom', horizontal: 'right'}}
+                        badgeContent={
+                            <SmallAvatar alt="Remy Sharp" src={iconPhoto}/>}>
+                        <Avatar alt="Travis Howard" src={avatar || userPhoto} sx={{width: 96, height: 96}}/>
+                    </Badge>
+                </Stack>
+                <div>
+                    {isEditMode
+                        ? <TextField
+                            onKeyPress={onKeyPressHandler}
+                            onChange={changeTitle}
+                            value={title}
+                            label="Nickname"
+                            variant="standard"
+                            autoFocus
+                            onBlur={activateViewMode}
+                            InputProps={{
+                                startAdornment: (<AccountCircle/>),
+                                endAdornment: (<ClearIcon/>)
+                            }}/>
+                        : <div className={styles.nickName}>Nickname:
+                            <div className={styles.name}>{title}</div>
+                            {!!activateViewMode && <ModeEditIcon onClick={activateEditMode} fontSize={'small'}/>}
+                        </div>}
+                </div>
+                <div className={styles.information}>
+                    <div className={styles.description}>Contact email: {email}</div>
+                    <div>Number of decks created: {publicCardPacksCount}</div>
+                </div>
+                <div>
+                    <Button className={styles.button} onClick={updateName}>Save</Button>
+                </div>
+            </div>
 
-					</div>
-				)
-			}
-		</div>
-	)
-}
+        </div>
+    );
+};
